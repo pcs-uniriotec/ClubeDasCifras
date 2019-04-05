456dd2203ce052fbe402599380275f2d11207f04
Daniel Coelho <daniel.coelho@uniriotec.br>
2019-04-05 15:15:11 -0300

    Adicionando caminho dinâmico para as páginas, a partir do __dirname

--- ProjetoClubeDasCifra/Backend/server.js
+++ ProjetoClubeDasCifra/Backend/server.js
@@ -1,9 +1,11 @@
 let express = require('express');//chamada para o express
 let app = express(); //bota o servidor de pé
-let port =3000; //porta do local host, para acessar o server
+let port = 3000; //porta do local host, para acessar o server
+let path = require('path');
+let caminho = path.join(__dirname, '../src');
 
 app.get('/', (req, res) => {
-    res.send('<h1>HOME KARALHO</h1>');
+    res.sendFile(caminho + '/pagInicial.html');
 })
 
 app.listen(port, () => {
