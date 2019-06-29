ccca0b6557664f206cf0e264ad3a96b44d08bc85
Daniel Coelho <daniel.coelho@uniriotec.br>
2019-06-28 23:24:35 -0300

    Revert "Adição de botão facebook"
    This reverts commit 946aefe7

--- ProjetoClubeDasCifra/src/js/pagInicial.js
+++ ProjetoClubeDasCifra/src/js/pagInicial.js
@@ -1,45 +1,45 @@
-(function () {
-    const elementoLogin = $('#login')
-    const elementoUsuario = $('#usuario')
-    const botaoDeslogar = $(':button[name="deslogar"]')
-    const botaoLogar = $('#logar')
-    const botaoCriarCifra = $(':button[name="cria-cifra"]')
-    const loginOpcoes = $('#login-opcoes')
-    const linkEnviarCifra = $('#enviar-cifra')
-    const linkCadastro = $('#cadastro')
-    const usuarioInputLogin = $('input[name="usuario"]')
-    const senhaInputLogin = $('input[name="password"]')
-    const botaoBuscaCifra = $('input[name="busca"]')
-    const inputBuscaMusica = $('#musica')
+(function(){
+    const elementoLogin       = $('#login')
+    const elementoUsuario     = $('#usuario')
+    const botaoDeslogar       = $(':button[name="deslogar"]')
+    const botaoLogar          = $('#logar')
+    const botaoCriarCifra     = $(':button[name="cria-cifra"]')
+    const loginOpcoes         = $('#login-opcoes')
+    const linkEnviarCifra     = $('#enviar-cifra')
+    const linkCadastro        = $('#cadastro')
+    const usuarioInputLogin   = $('input[name="usuario"]')
+    const senhaInputLogin     = $('input[name="password"]')
+    const botaoBuscaCifra     = $('input[name="busca"]')
+    const inputBuscaMusica    = $('#musica')
     const botaoLoginComGoogle = $('#google-login')
-    const botaoLoginComFacebook = $('#fb-login')
     const auth = firebase.auth();
     const db = firebase.firestore();
 
-    $(document).ready(function () {      //roda quando a pagina termina de carregar
+    $(document).ready(function() {      //roda quando a pagina termina de carregar
         verificaUsuario()
         buscaCifrasRankeadasPorNota()
         buscaCifrasRecentes()
+        buscaRankingUsuarios()
     })
 
     //update do firestore
-    db.settings({ timestampsInSnapshots: true });
+    db.settings({timestampsInSnapshots:true});
 
     function verificaUsuario() {                                     //verifica se usuario esta logado e conforme for faz alterações na página
         usuario = getUsuario()
 
-        if (!loginOpcoes.hasClass('invisible')) {
+        if(!loginOpcoes.hasClass('invisible')){
 
-            if (usuario !== undefined && usuario !== null) {  //entra no if se usuario tiver logado
+            if(usuario !== undefined && usuario !== null) {  //entra no if se usuario tiver logado
                 elementoLogin.addClass('invisible')                      //torna o login invisivel, pois usuario ja logou
                 linkCadastro.addClass('invisible')                     //torna invisivel link para cadastro
                 loginOpcoes.addClass('invisible')
                 botaoDeslogar.removeClass('invisible')                   //torna visivel botao de deslogar
                 linkEnviarCifra.removeClass('invisible')              //torna visivel link para enviar cifra
-                elementoUsuario.html('user: ' + usuario.usuario)     //define conteudo de elemento html com nome de usuario
+                elementoUsuario.html('user: '+ usuario.usuario)     //define conteudo de elemento html com nome de usuario
             }
-        } else {
-            if (usuario == null) {                                  //entra no if se usuario n tiver logado
+        }else {
+            if(usuario == null) {                                  //entra no if se usuario n tiver logado
                 linkEnviarCifra.addClass('invisible')                 //torna invisivel link para enviar cifra
                 elementoLogin.removeClass('invisible')                   //torna visivel o login
                 linkCadastro.removeClass('invisible')                  //torna visivel link para o cadastro
@@ -49,7 +49,7 @@
         }
     }
 
-    botaoDeslogar.click(function () {              //roda quando botao de deslogar é clicado
+    botaoDeslogar.click(function(){              //roda quando botao de deslogar é clicado
         botaoDeslogar.addClass('invisible')
         setUsuario(null)
         verificaUsuario()
@@ -57,17 +57,17 @@
 
 
 
-    botaoLogar.click(function () {                           //roda quando botao de login é clicado
-        let user = usuarioInputLogin.val()                         //pega nome do usuario atraves do input de login
+    botaoLogar.click(function() {                           //roda quando botao de login é clicado
+        let user     = usuarioInputLogin.val()                         //pega nome do usuario atraves do input de login
         let password = senhaInputLogin.val()                    //pega senha do usuario atraves do input de login
 
-        $.post("/login", { usuario: user, senha: password }, function (data) { //busca usuario com os respectivos nome e senha recebidos no input
+        $.post("/login", {usuario: user, senha: password}, function(data) { //busca usuario com os respectivos nome e senha recebidos no input
             setUsuario(data)
             verificaUsuario()
         })
     })
-
-    botaoBuscaCifra.click(function (e) {
+    
+    botaoBuscaCifra.click(function(e) {
         e.preventDefault()                                  //previne q a página recarregue
         let musica = inputBuscaMusica.val()                     //pega o valor inputado pelo usuário, no campo de nome de música
         setCifra(musica)                                    //seta o valor do elemento cifra do localStorage, com o nome de música passado pelo usuário
@@ -75,53 +75,34 @@
         window.location = '/cifra'                          //encaminha usuário para página de cifra
     })
 
-    botaoLoginComGoogle.click(function () {
-        var googleAuthProvider = new firebase.auth.GoogleAuthProvider()
+    botaoLoginComGoogle.click( function () {
+        var googleAuthProvider = new firebase.auth.GoogleAuthProvider
 
         return firebase.auth().signInWithPopup(googleAuthProvider)
-            .then(function (data) {
+            .then( function(data) {
                 console.log(data)
                 var idToken = data.credential.idToken
-                var usuarioAux = {
-                    nome: data.user.displayName, usuario: data.user.displayName, senha: "",
-                    email: data.user.email, favoritas: [], cifrasCriadas: [], usuariosSeguidos: []
-                }
+                var usuarioAux = {nome: data.user.displayName, usuario: data.user.displayName, senha:"",
+                                  email: data.user.email, favoritas: [], cifrasCriadas: [], usuariosSeguidos: []}
 
                 verificaExistenciaUsuario(usuarioAux)
 
             })
-            .catch(function (error) {
+            .catch( function(error) {
                 console.log(error)
             })
     })
 
-    botaoLoginComFacebook.click(function () {
-        var provider = new firebase.auth.FacebookAuthProvider();
-        return firebase.auth().signInWithPopup(provider).then(function (data) {
-            console.log(data)
-            var idToken = data.credential.idToken
-            var usuarioAux = {
-                nome: data.user.displayName, usuario: data.user.displayName, senha: "",
-                email: data.user.email, favoritas: [], cifrasCriadas: [], usuariosSeguidos: []
-            }
-
-            verificaExistenciaUsuario(usuarioAux)
-
-        })
-        .catch(function (error) {
-            console.log(error)
-        })
-    })
 
     function verificaExistenciaUsuario(usuarioAux) {
 
-        $.post('/verificaExistenciaUsuario', usuarioAux, function (data) {
+        $.post('/verificaExistenciaUsuario', usuarioAux, function(data) {
 
-            if (typeof data !== "object") {
-                $.post("/registro", usuarioAux, function (data) {
+            if(typeof data !== "object") {
+                $.post("/registro", usuarioAux, function(data) {
                 })
                 setUsuario(usuarioAux)
-            } else {
+            }else {
                 setUsuario(data)
             }
 
@@ -130,10 +111,10 @@
     }
 
     function buscaCifrasRankeadasPorNota() {
-        $.post('/buscaCifrasMelhoresNotas', {}, function (data) {
-            if (typeof data !== "object") {
+        $.post('/buscaCifrasMelhoresNotas', {}, function(data) {
+            if(typeof data !== "object") {
                 console.log("undefined")
-            } else {
+            }else {
                 console.log("ta entranu aqui")
                 console.log(data)
                 adicionaCifrasRankeadasPorNota(data)
@@ -143,7 +124,7 @@
 
     function adicionaCifrasRankeadasPorNota(data) {
         console.log(data)
-        for (let i = 0; i <= 4; i++) {
+        for(let i = 0; i <= 4; i++) {
             $(`
                 <br>
                 <a href="/cifra"  onclick="localStorage.setItem('cifra', '${data.rankingCifras[i].nome}')">
@@ -154,10 +135,10 @@
     }
 
     function buscaCifrasRecentes() {
-        $.post('/buscaCifrasRecentes', {}, function (data) {
-            if (typeof data !== "object") {
+        $.post('/buscaCifrasRecentes', {}, function(data) {
+            if(typeof data !== "object") {
                 console.log("undefined")
-            } else {
+            }else {
                 console.log(data)
                 adicionaCifrasRecentes(data)
             }
@@ -166,7 +147,7 @@
 
     function adicionaCifrasRecentes(data) {
         console.log(data)
-        for (let i = 0; i <= 4; i++) {
+        for(let i = 0; i <= 4; i++) {
             $(`
                 <br>
                 <a href="/cifra"  onclick="localStorage.setItem('cifra', '${data.cifrasRecentes[i].nome}')">
@@ -176,6 +157,33 @@
         }
     }
 
+    function buscaRankingUsuarios() {
+        console.log("AQUI 1")
+        $.post('/buscaRankingUsuarios', {}, function(data) {
+            console.log("OPA, MEU BOM")
+            if(typeof data !== "object") {
+                console.log("undefined")
+            }else {
+                adicionaRankingUsuarios(data)
+            }
+        })
+    }
 
+    function adicionaRankingUsuarios(data) {
+        console.log("OPA, AQUI TBM")
+        for(let i = 0; i <= 2; i++) {
+            $(`
+                <br>
+                <a href="/getVisitaPerfil"  onclick="localStorage.setItem('usuarioVisitado', '${data.ranking[i].nome}')">
+                ${data.ranking[i].nome} <br>
+                Quantidade de cifras: ${data.ranking[i].totalCifras}
+                </a>
+                <br>
+                
+            `).appendTo($('#melhores-colaboradores'))
+        }
+    }
+    
+    
 
 })();
